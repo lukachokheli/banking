f2e92ae26bafcf8bf1667bdfc2fa41b3d65d1759
lukachokheli <lukachokheli96@gmail.com>
2026-05-20 14:44:25 +0400

    Implement PlaidLink and Dwolla

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 
 const AuthForm = ({ type }: { type: string }) => {
@@ -47,7 +48,20 @@ const AuthForm = ({ type }: { type: string }) => {
       // Sign up with Appwrite and create plaid token
 
       if (type === 'sign-up') {
-        const newUser = await signUp(data);
+         const userData = {
+        firstName: data.firstName!,
+        lastName: data.lastName!,
+        address1: data.address1!,
+        city: data.city!,
+        state: data.state!,
+        postalCode: data.postalCode!,
+        dateOfBirth: data.dateOfBirth!,
+        ssn: data.ssn!,
+        email: data.email,
+        password: data.password,
+      }
+
+        const newUser = await signUp(userData);
 
         setUser(newUser);
       }
@@ -97,7 +111,9 @@ const AuthForm = ({ type }: { type: string }) => {
           </div>
         </header>
         {user ? (
-          <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
+          <div className="flex flex-col gap-4">
+            <PlaidLink user={user} variant="primary" />
+           </div>
         ) : (
           <>
             <Form {...form}>
@@ -124,7 +140,7 @@ const AuthForm = ({ type }: { type: string }) => {
 
                     <CustomInput
                       control={form.control}
-                      name="adress1"
+                      name="address1"
                       label="Address"
                       placeholder="Enter your specific address"
                     />
@@ -214,7 +230,7 @@ const AuthForm = ({ type }: { type: string }) => {
               </Link>
             </footer>
           </>
-        )}
+         )} 
       </section>
     );
   };
